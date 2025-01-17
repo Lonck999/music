1bc62cc34e3befa54e57419760386cd7d0896c95
地瓜球工程師 <joe22053814@gmail.com>
2025-01-17 17:46:16 +0800

    Update validation.js

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,11 +3,14 @@ import {
   Field as VeeField,
   ErrorMessage as VeeErrorMessage,
 } from "vee-validate";
+import { required } from "@vee-validate/rules";
 
 export default {
   install(app) {
     app.component("Form", VeeForm);
     app.component("Field", VeeField);
     app.component("ErrorMessage", VeeErrorMessage);
+
+    defineRule("required", required);
   },
 };
