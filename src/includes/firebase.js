7eaea49a251d505b8d38542d2cf2038b09b3a6f1
地瓜球工程師 <joe22053814@gmail.com>
2025-01-23 13:43:13 +0800

    Creact firebase

---
+++ src/includes/firebase.js
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyB6gqb03fUDnjwmaZwXQEsL4M4JrZcFoR8",
+  authDomain: "music-dee57.firebaseapp.com",
+  projectId: "music-dee57",
+  storageBucket: "music-dee57.firebasestorage.app",
+  messagingSenderId: "547399717579",
+  appId: "1:547399717579:web:945cb4cf4b87f77be0014e",
+};
+const app = initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import veeValidatePlugin from "./includes/validation";
+import "./includes/firebase";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(veeValidatePlugin);
+app.use(firebasePlugin);
 app.mount("#app");
