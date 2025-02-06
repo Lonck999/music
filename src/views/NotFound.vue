129ac62037610cd7d92e03b5b71f04446c5e6ce9
地瓜球工程師 <joe22053814@gmail.com>
2025-02-06 14:53:02 +0800

    Update NotFound.vue

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<script setup>
+import notFound from "../../public/assets/img/not-found.jpg";
+</script>
+<template>
+  <div>
+    <img :src="notFound" alt="not-found" />
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
