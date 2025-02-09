dc8ed2ea97d77b662ca1c47cab41213180dbc493
lonck <joe22053814@gmail.com>
2025-02-09 22:18:15 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import "./assets/base.css";
 import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import veeValidatePlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
-
 let app = null;
 auth.onAuthStateChanged((user) => {
   if (!app) {
