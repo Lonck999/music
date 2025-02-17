10f2d0cfd96d724c50c1f391b554c78dc14d9d9d
地瓜球工程師 <joe22053814@gmail.com>
2025-02-17 15:55:02 +0800

    Update Compositionltem.vue

--- src/components/Compositionltem.vue
+++ src/components/Compositionltem.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, ref } from "vue";
 
 const props = defineProps({
   song: {
@@ -7,12 +7,13 @@ const props = defineProps({
     required: true, // required是指這個屬性是必須的
   },
 });
+const showForm = ref(false);
 </script>
 <template>
   <!-- Composition Items -->
   <div class="border border-gray-200 p-3 mb-4 rounded">
-    <div>
-      <h4 class="inline-block text-2xl font-bold">Song Name</h4>
+    <div v-show="!showForm">
+      <h4 class="inline-block text-2xl font-bold">{{ song.modifiedName }}</h4>
       <button
         class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
       >
@@ -20,11 +21,12 @@ const props = defineProps({
       </button>
       <button
         class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+        @click.prevent="showForm = !showForm"
       >
         <i class="fa fa-pencil-alt"></i>
       </button>
     </div>
-    <div>
+    <div v-show="showForm">
       <form>
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
